ea44ccc6af5aafeea3276c33543449b526cdaa1f
Stiven Muñoz Murillo <stivenmm1993@gmail.com>
2022-01-12 14:59:53 -0500

    Siguiendo el ejemplo del vídeo anterior

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def agregar_arista(G, u, v, w=1, di=True):
 
     # Si el grafo no es dirigido
     if not di:
-        # Agrego la arista 
+        # Agrego otra arista en sentido contrario
         G.add_edge(v, u, weight=w)
 
 
@@ -18,8 +18,21 @@ if __name__ == '__main__':
     G = nx.DiGraph()
 
     # Add node/edge pairs
-    agregar_arista(G, "A", "B", 5, False)
+    agregar_arista(G, "A", "B", 5)
+    agregar_arista(G, "A", "D", 4)
+    agregar_arista(G, "A", "E", 2)
+    agregar_arista(G, "B", "C", 1, False)
+    agregar_arista(G, "B", "E")
+    agregar_arista(G, "C", "D", 3, False)
+    agregar_arista(G, "C", "F", 5)
+    agregar_arista(G, "D", "E", 3)
+    agregar_arista(G, "D", "F", 4)
+    agregar_arista(G, "E", "F", 8)
 
     # Draw the networks
-    nx.draw(G, with_labels=True)
+    pos = nx.layout.planar_layout(G)
+    nx.draw_networkx(G, pos)
+    labels = nx.get_edge_attributes(G, 'weight')
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+    plt.title("Grafo con NetworkX")
     plt.show()
